b18395ce40e010537248ca9ff72590a0eb622a98
ozirominervino-star <ozirominervino@gmail.com>
2026-04-24 22:13:46 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,17 @@
-const CACHE_NAME = "responde-facil-v1";
+const CACHE_NAME = "responde-facil-firebase-v2";
 const FILES = ["./", "./index.html", "./manifest.json"];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
 });
 
 self.addEventListener("fetch", (event) => {
